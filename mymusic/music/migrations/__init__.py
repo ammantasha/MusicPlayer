61b8878a531746e8d8099d033fb612a78e7323b1
MANTASHA SHAKEEL <62848573+ammantasha@users.noreply.github.com>
2020-08-02 13:03:57 +0530

    Create __init__.py

---
+++ mymusic/music/migrations/__init__.py
@@ -0,0 +1,2 @@
+The __init__.py files are required to make Python treat the directories as containing packages; this is done to prevent directories with a common 
+name, such as string, from unintentionally hiding valid modules that occur later on the module search path.
